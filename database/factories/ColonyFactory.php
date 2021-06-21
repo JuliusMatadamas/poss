35512e443f4d536f68e8997d6482e37ba70a82d8
JuliusMatadamas <julius3877@gmail.com>
2021-06-21 13:17:03 -0500

    Se crean los modelos, controladores y migraciones de inicio.

---
+++ database/factories/ColonyFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Colony;
+use Faker\Generator as Faker;
+
+$factory->define(Colony::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
